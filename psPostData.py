bdaa53dcdd98b3f54ef41ad95510843469331139
apkinto <apk.in.to@gmail.com>
2019-10-01 18:16:33 -0400

    Add files via upload

--- psPostData.py
+++ psPostData.py
@@ -10,35 +10,37 @@ def postData ( objectList, url ):
 		log.info('\tPOST StatusCode: %s\t Time: \t%s sec \t%s' % ( status, t, t ) )
 	
 def updateAttributeColors():
-	attributeValues = readExcel( excelData, 'AttributeValue' )
-	#print ( '===', attributeValues )
+	attributeValues = getExcelData( excelData, 'AttributeValue' )
 	for av in attributeValues:
+		print ('\n\n>>>>>>>')
 		''' For each row in the Excel, find the id's '''
 		av['OrganizationId'] = orgXref[ av['OrganizationCode'] ]
 		''' Make attribute call by organization to get attributeId '''
 		attrUrl = getUrl( psOrgUrl, str(av['OrganizationId']), 'child/attributes' )
 		psOrgAttr, t, status = getRest( attrUrl, session, payload, requestHeader, authorization, recordLimit, log )
+		#print ('excelSegmentCode', av['SegmentCode'])
 		for attr in psOrgAttr['items']:
+			#print ('attrSegmentCode', attr['SegmentCode'])
 			''' Process only SegmentCodes defined in the spreadsheet and get the AttributeId '''
 			if av['SegmentCode'] == attr['SegmentCode']:
-				print ("---", attr['SegmentCode'], attr['AttributeId'])
+				print ( "\t---", attr['SegmentCode'], attr['AttributeId'], '\n')
 				''' Get the system generated Key for Attribute '''
 				attrKey = getKey ( attr['links'] )
 				attrValUrl = getUrl(attrUrl, attrKey, 'child/attributeValues')
 				psOrgAttrVal, t, status = getRest( attrValUrl, session, payload, requestHeader, authorization, recordLimit, log )
+				print (psOrgAttrVal['items'])
 				for attrVal in psOrgAttrVal['items']:
+					print ( '******', attrVal['AttributeValueCode'], av['AttributeValueCode'] )
 					if attrVal['AttributeValueCode'] == av['AttributeValueCode']:
 						attrColor = {}
 						attrValKey = getKey ( attrVal['links'] )
 						postAttrValueUrl = getUrl(attrValUrl, attrValKey)
-						#print (postAttrValueUrl)
 						attrColor['OrganizationId'] = av['OrganizationId']
 						attrColor['AttributeId'] = attr['AttributeId']
 						attrColor['AttributeValueId'] = attrVal['AttributeValueId']
 						attrColor['Color'] = av['Color']
 						t, status, statusText = patchRest( postAttrValueUrl, session, attrColor, requestHeader, authorization, log )
-						#print ( attrColor )
-						
+	print ('<<<<<<<')					
 						
 if __name__ == "__main__":
 	
@@ -79,12 +81,12 @@ if __name__ == "__main__":
 	'''
 	
 	'''  Read data from Excel worksheet '''
-	plans = readExcel( excelData, 'Plans' )
+	plans = getExcelData( excelData, 'Plans' )
 	
 	'''	For each plan, insert OrgId	'''
 	#print (plans)
 	for plan in plans:
-		plan.pop('PlanId')
+		#plan.pop('PlanId')
 		plan['OrganizationId'] = orgXref[ plan['OrganizationCode'] ]
 	#print ( plans )
 	
